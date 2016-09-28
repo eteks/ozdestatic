91b218e705b9db789572b20b442ec0f318f3d8c2
spmuthu21 <muthu@globalensolutions.com>
2016-09-28 10:44:55 +0530

    initial commit on oozaaoo backup

---
+++ contact.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+      <title>Oozaaoo | Contact Us</title>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+      <link rel="icon" href="favicon.ico" type="image/x-icon">
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+      <link href="css/style-m.css" rel="stylesheet">
+      <link href="css/style-t.css" rel="stylesheet">
+	  <link href="css/jcarousel.responsive.css" rel="stylesheet" >
+       <link href="css/header-css/style5.css" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        
+    <script type="text/javascript" src="js/modernizr.custom.28468.js"></script>
+    
+    <noscript>
+      <link rel="stylesheet" type="text/css" href="css/nojs.css" />
+    </noscript>
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+      <![endif] data-spy="scroll" data-target=".navbar" data-offset="79"   data-offset-bottom = "200" -->
+   </head>
+   <body>
+      <section class="oza-header">
+         <header id="navbar-top" class="banner navbar" role="banner">
+            <section class="pannello-affix affix-top" data-spy="affix" data-offset-top = "60">
+               <section class="container">
+                  <section class="navbar-inner">
+                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                     <span class="sr-only">Toggle Navigation</span>
+                     <span class="icon-bar glyphicon glyphicon-tasks"></span>
+                     </button>
+                     <a class="brand" title="Oozaaoo" href="index.html"><img class="logo" alt="Oozaaoo" src="images/logo.png"></a>   
+                     <nav class="collapse navbar-collapse  oza-sidebar-offcanvas" role="navigation">
+                        <ul class="nav navbar-nav oza-header-menu-nav text-uppercase">
+                           <li class="menu-home active"><a href="index.php" title="Home">Home</a></li>
+                           <li class="menu-about-us"><a href="about_us.php" title="About Us">About Us</a></li>
+                           <li class="menu-domestic"><a href="domestic.php" title="Inbound">Inbound</a></li>
+                           <li class="menu-international"><a href="international.php" title="Outbound">Outbound</a></li>
+                           <li class="menu-contact-us"><a href="contact.php" title="Contact Us">Contact Us</a></li>
+                        </ul>
+                     </nav>
+                  </section>
+               </section>
+            </section>
+         </header>
+         
+
+
+<section class="container-fluid visible-xs mobile-header">
+        <section class="row">
+          <article class="col-xs-12 mobile-column1-contact">
+            <div class="header-slide-content-contact">
+                <p class="header-offer1-contact"> Amazing </p>
+                <p class="header-holiday1-contact"> Tours for Your Dream Vacation <mark id="header-fade-content1">Dream Vacation </mark></h3>
+                
+                
+              </div>
+             
+          </article>
+        </section>
+        <section class="row">
+          <article class="col-xs-6 mobile-column2-contact">
+            
+          </article>
+          <article class="col-xs-6 mobile-column3-contact">
+            <img class="img-responsive" src="images/contact1.png" alt="image01"/>
+          </article>
+        </section>
+      </section>
+
+
+
+
+<section class="hidden-xs">
+        <div id="da-slider" class="container-fluid da-slider animation_cloud">
+          
+          <div class="da-slide">
+            <h2>
+              <div class="image_three_boat">
+                <img src="images/contact2.png" alt="image01" />
+              </div>
+            </h2>
+            <!-- <p> </p> -->
+            <div class="da-link right-mountain-people">
+              
+            </div>
+            <div class="image_one_mountain">
+              <div class="header-slide-content">
+                <p class="header-offer1"> Amazing </p>
+                <p class="header-holiday1"> Tours for Your Dream Vacation <mark id="header-fade-content1">Dream Vacation </mark></h3>
+                
+                
+              </div>
+             
+                
+              </div>
+            </div>
+            <div class="da-img image_sea">
+              <img src="images/contact1.png" alt="image01" />
+            </div>
+          </div>
+            <!-- <div class="da-slide">
+              <h2>Quality Control</h2>
+              <p>Even the all far World of Grammar.</p>
+              <a href="#" class="da-link">Read more</a>
+              <div class="da-img"><img src="images/4.png" alt="image01" /></div>
+            </div> -->
+            <!-- <nav class="da-arrows">
+              <span class="da-arrows-prev"></span>
+              <span class="da-arrows-next"></span>
+            </nav> --> 
+        </div>
+      </section> <!-- Animation section end -->
+    </section>
+
+
+
+
+
+
+<?php include("pop_up.php"); ?>
+
+
+
+
+
+
+      <main >
+	  
+            <section class="container oza-container">
+<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+<h3 class="oza-contact-title oza-contact-locate">Locate us</h3>
+<p>No 7, Chandrasekaran Street Iyyanar Nagar, Pillaitottam, Pondicherry - 605013</p>
+<button class="btn btn-success oza-bg-none oza-border-zero oza-text-success">MAP</button>
+</article>
+
+
+<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+<h3 class="oza-contact-title oza-contact-call">Call us</h3>
+<p>9360206020</p>
+<!-- <p>+91 413-4300008 / 4300001 / 4300003</p>
+ --><!-- <p>+91 9360206020 / 90039997766</p> -->
+
+</article>
+
+
+<article class="col-xs-12 col-sm-12 col-md-6 col-lg-6 text-center col-md-offset-3 col-md-offset-3">
+<article class="show">
+<h3 class="oza-contact-title oza-contact-message">Message</h3>
+</article>
+<section class="oza-modal-contact-content">
+         <form id="contact_form" class="form-inline">
+<article class="form-group col-xs-12 col-sm-12 coll-md-12 col-lg-12 ">
+               <label class="sr-only" for="oza-modal-name">Write your name here<sup>*</sup></label>
+               <input type="text" class="form-control" id="oza-modal-name" name="Name" placeholder="Write your name here*">
+            </article>
+			
+<article class="form-group col-xs-12 col-sm-12 coll-md-12 col-lg-12">
+               <label class="sr-only" for="oza-modal-contact">How to contact you?<sup>*</sup></label>
+               <input type="email" class="form-control" id="oza-modal-contact" name="Email" placeholder="How to contact you?*">
+            </article>
+			
+<article class="form-group col-xs-12 col-sm-12 coll-md-12 col-lg-12">
+               <label class="sr-only" for="oza-textarea">Write your message here*</label>
+               <textarea class="form-control" rows="3" id="oza-textarea" name="Msg" placeholder="Write your message here*"></textarea>
+            </article>
+
+<article class="form-group col-xs-12 col-sm-12 coll-md-12 col-lg-12 text-center">
+<button class="btn btn-success oza-bg-none oza-border-zero oza-text-success" type="submit">SEND</button>
+</article>
+
+</article>
+
+<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+<a href="#" class="oza-contact-title oza-contact-facebook oza-text-success">Find us on facebook</a>
+</article>
+
+
+<article class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
+<a href="#" class="oza-contact-title oza-contact-google oza-text-success">Find us on google plus</a>
+</article>
+
+</section>
+         
+      </main>
+       <?php include("footer.php"); ?>
+	   
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+      <script src="js/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include insectionidual files as needed -->
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/bootstrap-affix.js"></script>
+      <script src="js/custom.js"></script>
+          <script type="text/javascript" src="js/jquery.cslider.js"></script>
+    <script type="text/javascript">
+      $(function() {
+      
+        $('#da-slider').cslider({
+         autoplay  : true,
+         
+          //bgincrement : 450
+        });
+      
+      });
+    </script>
+	  
+<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script> 
+<script type="text/javascript" src="js/jcarousel.responsive.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+ <link href="css/jquery-ui.css" rel="stylesheet">
+        <script src="js/jquery-ui.min.js"></script> 
+        <script>
+        $('#datepicker, #datepicker_depature').datepicker({
+            dateFormat: 'MM dd, yy',
+            minDate: 0,
+            showOtherMonths: true
+        });
+      </script>
+      <script src="js/validation.js"></script>     
+   </body>
+</html>
