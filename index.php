bd93d222a4562974b914403396e1c275d871304c
bala.2595 <etsint_bala@etekchnoservices.com>
2016-09-28 20:03:46 +0530

    Kindly check it once and move to server.

--- index.php
+++ index.php
@@ -328,7 +328,7 @@
       </section>
         <div id="carouselHome" class="carousel slide carousel-fade">
           <div class="carousel-inner">
-            <div class="active item" >
+            <div class="container active item" >
               <img class="client-test" src="images/footer-slider/footer1.png" alt="Lifestyle" />
               <section class="container">
                 <section class="row">
@@ -341,7 +341,7 @@
                 </section>
               </section>
             </div>
-            <div class="item">
+            <div class="container item">
               <img class="client-test" src="images/footer-slider/footer2.png" alt="Cocktails" />
               <section class="container">
                 <section class="row">
@@ -354,7 +354,7 @@
                 </section>
               </section>
             </div>
-            <div class="item">
+            <div class="container item">
               <img class="client-test" src="images/footer-slider/footer3.png" alt="Cocktails" />
                 <section class="container">
                   <section class="row">
@@ -367,7 +367,7 @@
                    </section>
                 </section>
             </div>
-             <div class="item">
+             <div class="container item">
                 <img class="client-test" src="images/footer-slider/footer4.png" alt="Cocktails" />
                 <section class="container">
                    <section class="row">
@@ -465,4 +465,40 @@
       
       <?php include("knowmore_popup.php"); ?>    
   </body>
+  
+      <footer id="navbar-bottom" class="oza-footer" role="banner">
+      <section class="container">
+         <section class="col-xs-12 col-sm-12 coll-md-10 col-lg-10 oza-book-tour">
+            <section class="col-sm-6 col-md-6 col-xs-11 oza-book-tour-contact">
+              <section class="dl">
+                <section class="brand">
+                  <h4>Book your tour package now!</h4>
+                </section>
+                <section class="discount alizarin">
+                  <section class="type">
+                    <h3>+91 90039 99766</h3>
+                   <h6> MAIL ID: info@oozaaoo.com</h6>
+                </section>
+            </section>
+      </section>
+      </section>
+         </section>
+         <section class="navbar-inner hidden-xs">
+            <nav role="navigation">
+               <ul class="nav navbar-nav oza-footer-menu-nav text-uppercase">
+                  <li class="oza-menu active"><a href="index.php" title="Home">Home</a></li>
+                  <li class="menu-about-us"><a href="about_us.php" title="About Us">About Us</a></li>
+                  <li class="menu-domestic"><a href="domestic.php" title="Inbound">Inbound</a></li>
+                  <li class="menu-international"><a href="international.php" title="Outbound">Outbound</a></li>
+                  <li class="menu-contact-us"><a href="contact.php" title="Contact Us">Contact Us</a></li>
+               </ul>
+            </nav>
+         </section>
+         <section class="col-xs-12 col-sm-12 coll-md-12 col-lg-12 oza-copy-right">
+            <p class="custom_font">Copyright &copy; Oozaaoo. All rights reserved.</p>
+         </section>
+      </section>
+      </footer>
+
+            
 </html>
